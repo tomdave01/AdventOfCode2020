e192dccb32e2833dfbd645a9c75291e69a2b5808
tomdave01 <tomdave01@gmail.com>
2020-12-02 15:09:25 -0700

    Complete solution of Day 2 Part 1

--- Day2.py
+++ Day2.py
@@ -1,18 +1,17 @@
 import re
 
 def PasswordScreener(file):
+    second_counter = 0
     with open(file, 'r') as file:
         for line in file:
             line = line.rstrip()
             parameters = re.compile(r'\w+')
             parameters = parameters.findall(line)
-
             counter = 0
-            second_counter = 0
             for char in parameters[3]:
                 if char == parameters[2]:
-                    counter +=1
-            if (counter >= int(parameters[0])) or (counter <= int(parameters[1])):
+                    counter += 1
+            if (counter >= int(parameters[0])) and (counter <= int(parameters[1])):
                 second_counter += 1
 
     return second_counter
